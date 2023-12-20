5c738ab441f3bbf67d817bc131c652e33ec6926b
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2023-12-20 15:08:29 +0100

    helloWorld and variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    String salutation = "Hi there! My name is Jesus";
+    System.out.println(salutation);
+    System.out.println(salutation.toUpperCase());
+  }
+
+}
