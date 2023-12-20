934ae9ec1429f7ac78d454271f53cfb8c1193785
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2023-12-20 16:43:49 +0100

    conditional

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,19 @@ public class HelloWorld {
     String salutation = "Hi there! My name is Jesus";
     System.out.println(salutation);
     System.out.println(salutation.toUpperCase());
-  }
 
+    int number = 10;
+    System.out.println(number);
+    boolean valor = true;
+
+    if(valor){
+      number = 5;
+      System.out.println("The new value of the number is " +number);
+    }
+
+    String name;
+    if(number>10){
+      name = "Alfredo";
+    }
+  }
 }
