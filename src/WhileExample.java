1b779a37b8e97dd591d3f398341ca11e5f049805
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-01 19:23:24 +0100

    changes in file

--- src/WhileExample.java
+++ src/WhileExample.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class WhileExample {
+public class WhileExample {
+
+  public static void main(String[] args) {
+    int x = 101;
+        while(x<100){
+          System.out.println(x++);
+        }
+        System.out.println("index out of bound");
+  }
 }
