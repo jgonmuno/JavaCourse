7b39510e7db287ef08b9d19f7367c467913fe09c
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-04 12:22:24 +0100

    tables

---
+++ src/PracticeFor1.java
@@ -0,0 +1,15 @@
+public class PracticeFor1 {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 20; i++) {
+      System.out.println("imprimiendo tabla del " +i+ "\n");
+
+      for (int j = 0; j <=20; j++) {
+        System.out.println(i+"x"+j+"="+(i*j) + "\n");
+      }
+    }
+
+  }
+
+}
