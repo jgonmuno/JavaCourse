bc0f97bf9f66defa9c2cca58cc82eba6c38d6c14
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-08 13:39:31 +0100

    forExample

--- src/ForExample.java
+++ src/ForExample.java
@@ -2,7 +2,7 @@ public class ForExample {
 
   public static void main(String[] args) {
 
-    outer : for (int j = 0; j < 10; j++) {
+    outer : for (int j = 0; j < 20; j++) {
       System.out.println("Imprimiendo tabla del " +j+"\n");
      inner: for (int i = 0; i < 10; i++) {
         if (i%2==0){
