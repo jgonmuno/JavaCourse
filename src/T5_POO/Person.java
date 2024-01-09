9ce1ffcdfa84220107c72f0211f8c0e0dd9ed408
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-09 17:54:11 +0100

    parameters

--- src/T5_POO/Person.java
+++ src/T5_POO/Person.java
@@ -2,28 +2,38 @@ package T5_POO;
 
 public class Person {
 
+
+  String name;
+  int age;
+  String gender;
+
+  void playVideogames(Person person){
+    System.out.println(name+ " is playing with " +person.name);
+  }
+
+  void printInformation(){
+    System.out.println("Name: " +name);
+    System.out.println("Age: " +age);
+    System.out.println("Gender: " +gender);
+  }
+
+
   public static void main(String[] args) {
     Person person1 = new Person();
     person1.name = "Jesus";
     person1.age = 33;
     person1.gender = "Male";
     person1.printInformation();
-
+    
     Person person2 = new Person();
     person2.name = "Fernando";
     person2.age = 32;
     person2.gender = "Male";
     person2.printInformation();
-  }
 
-  String name;
-  int age;
-  String gender;
+    person1.playVideogames(person2);
 
-  void printInformation(){
-    System.out.println("Name: " +name);
-    System.out.println("Age: " +age);
-    System.out.println("Gender: " +gender);
+
   }
 
 }
