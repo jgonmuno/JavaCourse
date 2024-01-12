26ab509bfb915e06252a9a2e0da466ee1c8840a9
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-12 11:43:46 +0100

    modifications in dog class

--- src/T5_POO/Pet/Dog.java
+++ src/T5_POO/Pet/Dog.java
@@ -17,12 +17,22 @@ public class Dog extends Pet {
   public static void main(String[] args) {
     Dog letiDog = new Dog("Pancho", 11, "White", "Labrador");
     letiDog.printInformation();
+
     Dog anaDog = new Dog("Nera", 7, "Black", "Malinois");
     anaDog.printInformation();
+
     Dog angelDog = new Dog ("Calcetines", 2, "White", "Chihuahua");
     angelDog.printInformation();
+
     Dog juanfranDog = new Dog("Atila", 8, "Black", "German shepherd");
     juanfranDog.printInformation();
+
+    Dog javiDog = new Dog("Pepe", 5, "White", "Dogo");
+    javiDog.printInformation();
+
+    Dog fernandoDog = new Dog("Cristiano", 6, "White", "Carlino");
+    fernandoDog.printInformation();
+
   }
 
 }
