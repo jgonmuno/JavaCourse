8bd109b36e877451b615b6c34d10ad386002039b
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-12 11:44:06 +0100

    added cat class

---
+++ src/T5_POO/Pet/Cat.java
@@ -0,0 +1,24 @@
+package T5_POO.Pet;
+
+public class Cat extends Pet {
+
+  String eyes;
+  String species;
+
+  public Cat(String name, int age, String color, String species, String eyes){
+    super(name, age, color);
+    this.species = species;
+    this.eyes = eyes;
+  }
+
+  void printInformation(){
+    System.out.println("My friend has a cat named " +name + " aged " +age+ " of " +species+ " species with " +eyes+ " eyes");
+  }
+
+  public static void main(String[] args) {
+    Cat seteCat=new Cat("Elfa", 3, "black", "angora", "green");
+    seteCat.printInformation();
+  }
+
+
+}
