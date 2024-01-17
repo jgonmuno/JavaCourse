0952bf60fb4a34378b995d66fba4634755026b54
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-17 14:09:43 +0100

    modifiers example

---
+++ src/T5_POO/modifiers/ExamplePrivate.java
@@ -0,0 +1,14 @@
+package T5_POO.modifiers;
+
+class Test{
+ private String name;
+
+  public static void main(String[] args) {
+    Test t = new Test();
+    t.name = "Daniel";
+  }
+}
+
+public class ExamplePrivate {
+
+}
