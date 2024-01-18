402c4410d8e665f6c340ffb19c25c95d72e39352
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-18 09:33:58 +0100

    encapsuling

---
+++ src/T6/encapsuling/Exam.java
@@ -0,0 +1,35 @@
+package T6.encapsuling;
+
+class Exam {
+   private double mark;
+   private int questions;
+   private boolean active;
+
+   public void setMark(double mark) throws Exception {
+      this.mark = mark;
+
+      if (this.mark < 0){
+         throw new Exception("Incorrect mark, please insert a mark above or equal to 0");
+      }
+   }
+
+   public double getMark(){
+      return mark;
+   }
+
+   public void setQuestions(int questions){
+      this.questions = questions;
+   }
+
+   public int getQuestions(){
+      return questions;
+   }
+
+   public boolean isActive() {
+      return active;
+   }
+
+   public void setActive(boolean active) {
+      this.active = active;
+   }
+}
---
+++ src/T6/encapsuling/ExamTest.java
@@ -0,0 +1,14 @@
+package T6.encapsuling;
+
+public class ExamTest {
+
+  public static void main(String[] args) throws Exception {
+    Exam exam = new Exam();
+
+    exam.setMark(5.4);
+    exam.getMark();
+  }
+
+
+
+}
