6b220a891424ba292620ec9ed67f356e5800d5d5
jgonmuno <jesus.gonzalezmunoz@emeal.nttdata.com>
2024-01-18 18:05:05 +0100

    added test class

---
+++ src/T7_polimorfism/FiguresTest.java
@@ -0,0 +1,16 @@
+package T7_polimorfism;
+
+public class FiguresTest {
+
+  void printArea (Figures figure){
+    System.out.println("The color of the figure is " +figure.getColor()+ " and its area of " +figure.calculateArea());
+  }
+
+  public static void main(String[] args) {
+    FiguresTest figuresTest = new FiguresTest();
+    figuresTest.printArea(new Circle("Red", 4.0));
+    figuresTest.printArea(new Square("Blue", 8.0));
+    figuresTest.printArea(new Rectangle("Black", 3.3, 2.8));
+  }
+
+}
